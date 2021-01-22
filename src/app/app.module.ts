a9c99b6804f2861df83524950fd4822a44507f2c
newbrine <newbrine@gmail.com>
2021-01-22 09:01:19 -0600

    initial navbar commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { LoginComponent } from './pages/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { RegisterComponent } from './pages/register/register.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +15,8 @@ import { RegisterComponent } from './pages/register/register.component';
     HomeComponent,
     LandingComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    NavbarComponent
    
   ],
   imports: [
