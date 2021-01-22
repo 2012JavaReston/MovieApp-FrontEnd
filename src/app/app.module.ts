84ad9e05093d9d572fffdb71d9d6ea633187e7f4
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2021-01-21 16:17:09 -0800

    Set up routing to home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
