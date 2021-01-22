c0606be7c0977f671a1433a1b677abd6274e397a
Lester Young <lestermyoung0789@gmail.com>
2021-01-21 21:40:19 -0500

    fixing conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 const routes: Routes = [
+  {path: '', component: LandingComponent},
+  { path: 'login', component: LoginPageComponent}, 
+  { path: 'register', component: RegisterPageComponent},
   { path: 'home', component: HomeComponent },
+  
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
     LandingComponent,
     LoginComponent,
     RegisterComponent,
+    LoginPageComponent, 
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
