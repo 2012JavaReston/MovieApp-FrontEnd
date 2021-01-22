2e1a30bf599e7ab7225856dac3e236d564550b12
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2021-01-21 17:59:59 -0800

    Connect html input to ts variable

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  search: String = "";
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  clickSearch(): void {
+    this.router.navigate(['/search', this.search]);
   }
 
 }
