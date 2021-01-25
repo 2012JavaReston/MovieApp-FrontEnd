4c82f61209fd8a851cc03753a1a63768455bf4ed
Alec Sherlock <ahsherlock0@gmail.com>
2021-01-25 12:38:33 -0800

    adding some css

--- src/app/services/tmdb.service.ts
+++ src/app/services/tmdb.service.ts
@@ -4,37 +4,34 @@ import { Movie } from '../interfaces/Movie';
 import { ApiKey } from '../../secrets/ApiKey';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TmdbService {
-  private baseUrl = 'https://api.themoviedb.org/3/search/movie?api_key=' + ApiKey + '&query=';
-  
-  constructor(
-    private http: HttpClient,
-    ) { }
+  private baseUrl =
+    'https://api.themoviedb.org/3/search/movie?api_key=' + ApiKey + '&query=';
 
-  public getMovies(url: string): Movie[]{
+  constructor(private http: HttpClient) {}
+
+  public getMovies(url: string): Movie[] {
     let movies: Movie[] = [];
-    this.http.get<string>(this.baseUrl + url).subscribe(
-      (data: any) => {
-        let jsonMovies: any = data["results"];
-        jsonMovies.forEach((jsonMovie: any) => {
-          let movie: Movie = {
-            title: '',
-            description: '',
-            image: '',
-            releaseDate: ''
-          }
-          if(jsonMovie["poster_path"] != null){
-            movie.title = `${jsonMovie["title"]}`;
-            movie.description = `${jsonMovie["overview"]}`;
-            movie.image = `https://image.tmdb.org/t/p/original${jsonMovie["poster_path"]}`;
-            movie.releaseDate = `${jsonMovie["release_date"]}`;
-            movies.push(movie);
-          }
-        });
-      }
-    );
+    this.http.get<string>(this.baseUrl + url).subscribe((data: any) => {
+      let jsonMovies: any = data['results'];
+      jsonMovies.forEach((jsonMovie: any) => {
+        let movie: Movie = {
+          title: '',
+          description: '',
+          image: '',
+          releaseDate: '',
+        };
+        if (jsonMovie['poster_path'] != null) {
+          movie.title = `${jsonMovie['title']}`;
+          movie.description = `${jsonMovie['overview']}`;
+          movie.image = `https://image.tmdb.org/t/p/original${jsonMovie['poster_path']}`;
+          movie.releaseDate = `${jsonMovie['release_date']}`;
+          movies.push(movie);
+        }
+      });
+    });
     return movies;
   }
 }
