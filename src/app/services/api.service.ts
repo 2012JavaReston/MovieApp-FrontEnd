f575026a8dfb7e314a11e69644273415be30c59c
Alec Sherlock <ahsherlock0@gmail.com>
2021-01-25 14:25:13 -0800

    added user registration functionality. new users can now be registered into the application

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -4,33 +4,48 @@ import { Router } from '@angular/router';
 import { User } from '../interfaces/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
   private baseUrl = 'http://localhost:8080/MovieApp/api/';
 
+  constructor(private http: HttpClient, private router: Router) {}
 
-  constructor(private http: HttpClient, private router: Router) { }
+  loginUser(user: User) {
+    let message = '';
+    console.log(user);
+    this.http
+      .post<User>(`${this.baseUrl}/user/login`, user)
+      .subscribe((data) => {
+        console.log(data);
+        if (data != null) {
+          let userStore = JSON.stringify(data);
+          console.log(userStore);
+          //storing user in local storage? idk if we need it.
+          localStorage.setItem('user', userStore);
+          this.router.navigate(['home']);
+        } else {
+          console.log('here');
+          message = 'Sorry wrong Username or Password.';
+        }
+      });
 
-  loginUser(user: User){
-    let message = ""; 
-    console.log(user); 
-    this.http.post<User>(`${this.baseUrl}/user/login`, user).subscribe(data => {
-      console.log(data); 
-      if(data!=null){
-        let userStore = JSON.stringify(data); 
-        console.log(userStore); 
-        //storing user in local storage? idk if we need it. 
-        localStorage.setItem("user", userStore); 
-        this.router.navigate(['home']);   
-      } else {
-        console.log("here"); 
-        message = "Sorry wrong Username or Password."; 
-
-      } 
-    })
-     
-    return message; 
-    
+    return message;
+  }
+  registerNewUser(newUser: User) {
+    let message = '';
+    console.log(newUser);
+    this.http
+      .post<User>(`${this.baseUrl}/user/register`, newUser)
+      .subscribe((data) => {
+        console.log(data);
+        if (data != null) {
+        } else {
+          console.log('USER WAS REGISTERED');
+          message = 'USER WAS REGISTERED';
+          this.router.navigate(['login']);
+        }
+      });
+    return message;
   }
 }
