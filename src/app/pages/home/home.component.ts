991926ed1c636b6b65a13ce6f01081a3a16443fb
agee11 <a.gee5@yahoo.com>
2021-01-29 14:42:41 -0800

    Add random movie generator button to home screen

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { generate } from 'rxjs';
 import { TmdbCollections } from 'src/app/interfaces/TmdbCollections';
+import { TmdbService } from 'src/app/services/tmdb.service';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +13,7 @@ export class HomeComponent implements OnInit {
   search: String = '';
   collection: any = TmdbCollections;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private tmdb: TmdbService) {}
 
   ngOnInit(): void {}
 
@@ -19,4 +21,23 @@ export class HomeComponent implements OnInit {
     console.log(`SEARCH: ${this.search}`);
     this.router.navigate(['/search', this.search]);
   }
+
+  generateRandomMovie(): void {
+    let movieId = Math.floor(Math.random() * 1000000);  //range from 0-999999
+    this.tmdb.getMovieById(movieId).subscribe(
+      (data:any) => {
+        if(data && !data.adult && data.poster_path){
+          this.router.navigate([`movieInfo/${movieId}`]);
+        }else{
+          //If movie is adult or lacks an image, search again!
+          this.generateRandomMovie();
+        }
+      }, error => {
+        //If id is invalid, search again!
+        this.generateRandomMovie();
+      }
+    )
+    
+  }
+
 }
