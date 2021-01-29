eeac330993c7f906ca9992ab018d110bf3eb4c6a
newbrine <newbrine@gmail.com>
2021-01-29 15:58:42 -0600

    e2e testing for landing page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -35,11 +35,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('MovieApp');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('MovieApp app is running!');
-  });
 });
